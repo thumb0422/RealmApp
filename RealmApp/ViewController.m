16a6af1b230abe59b831c0fc4791bf095de3d3ab
liuchunhua <thumb0422@163.com>
2017-12-05 21:46:26 +0800

    update

--- RealmApp/ViewController.m
+++ RealmApp/ViewController.m
@@ -8,15 +8,47 @@
 
 #import "ViewController.h"
 #import <AFNetworking/AFNetworking.h>
+#import <objc/runtime.h>
 
-@interface ViewController ()
+@interface OrderMain:NSObject
+@property (nonatomic,copy) NSString *orderId;
+@property (nonatomic,copy) NSString *amount;
+@property (nonatomic,copy) NSString *count;
+@property (nonatomic,copy) NSString *states;
+- (NSDictionary *)properties_aps;
+@end
+
+@implementation OrderMain
+
+- (NSDictionary *)properties_aps
+{
+    NSMutableDictionary *props = [NSMutableDictionary dictionary];
+    unsigned int outCount, i;
+    objc_property_t *properties = class_copyPropertyList([self class], &outCount);
+    for (i = 0; i<outCount; i++)
+    {
+        objc_property_t property = properties[i];
+        const char* char_f =property_getName(property);
+        NSString *propertyName = [NSString stringWithUTF8String:char_f];
+        id propertyValue = [self valueForKey:(NSString *)propertyName];
+        if (propertyValue)
+            [props setObject:propertyValue forKey:propertyName];
+    }
+    free(properties);
+    return props;
+}
+
+@end
+
+@interface ViewController (){
+    OrderMain *orderMain;
+}
 @property (weak, nonatomic) IBOutlet UITextField *ipText;
 @property (weak, nonatomic) IBOutlet UITextField *portText;
 @property (weak, nonatomic) IBOutlet UIButton *getBtn;
 - (IBAction)getBtnClick:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextField *funcText;
 - (IBAction)postBtnClick:(id)sender;
-
 @end
 
 @implementation ViewController
@@ -24,6 +56,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    orderMain = [[OrderMain alloc] init];
+    orderMain.amount = @"120";
+    orderMain.states = @"Y";
+    orderMain.count  = @"10";
 }
 
 - (IBAction)getBtnClick:(id)sender {
@@ -37,6 +73,20 @@
 }
 - (IBAction)postBtnClick:(id)sender {
     NSString *urlStr = [NSString stringWithFormat:@"%@:%@/%@",_ipText.text,_portText.text,_funcText.text];
+    
+    NSDictionary *params = [orderMain properties_aps];
+    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    
+    session.responseSerializer = [AFJSONResponseSerializer serializer];
+    session.requestSerializer=[AFJSONRequestSerializer serializer];
+    session.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/json",@"application/json",nil];
+    
+    [session POST:urlStr parameters:params success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"请求成功%@",responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败");
+    }];
+    /**
     AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
     [session POST:urlStr parameters:[NSDictionary new] progress:^(NSProgress * _Nonnull uploadProgress) {
         
@@ -57,6 +107,7 @@
         }
     }];
     [dataTask resume];
+    **/
 }
 
 @end
